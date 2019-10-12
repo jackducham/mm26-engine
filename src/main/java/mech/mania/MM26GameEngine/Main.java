5fd541a4f74f68d1e00f74104d3370e5d4719f65
Sahil Patel <sahilshahpatel@gmail.com>
2019-10-12 18:02:09 -0500

    Added javax.websocket and protobuf to maven dependencies. We may end up using a different websocket library instead. Downloaded protobuf binary, but since it is OS dependent, I added it to the gitignore.

--- src/main/java/mech/mania/MM26GameEngine/Main.java
+++ src/main/java/mech/mania/MM26GameEngine/Main.java
@@ -26,6 +26,7 @@ public class Main {
 		// Begin game loop
 		while(!gameOver){
 			// TODO
+			//System.out.println("Game is running.");
 		}
 	}
 
---
+++ src/main/java/mech/mania/MM26GameEngine/visualizerCommunication/VizualizerWebSocket.java
@@ -0,0 +1,7 @@
+package mech.mania.MM26GameEngine.visualizerCommunication;
+
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "")
+public class VizualizerWebSocket {
+}
