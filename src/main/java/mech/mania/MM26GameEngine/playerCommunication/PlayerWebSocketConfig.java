8b6fe405ff8a7d588c01c0d469467d0616de7be4
Shaw Kagawa <skagawa2@illinois.edu>
2019-10-19 17:06:20 -0500

    saw that websocket with spring was already implemented, copied code into playerCommunication package

---
+++ src/main/java/mech/mania/MM26GameEngine/playerCommunication/PlayerWebSocketConfig.java
@@ -0,0 +1,26 @@
+package mech.mania.MM26GameEngine.playerCommunication;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+public class PlayerWebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/player")
+                .addInterceptors(new HttpSessionHandshakeInterceptor()); //TODO: set correct endpoint
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new PlayerBinaryWebSocketHandler();
+    }
+
+}
