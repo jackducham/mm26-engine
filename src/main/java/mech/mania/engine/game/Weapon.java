22454db930b0eb09234b1438b92e8adad172c66b
werdwar <drewlw2@illinois.edu>
2019-10-26 17:29:36 -0500

    Updated the contents of Weapon and Tile to match the issue.

--- src/main/java/mech/mania/engine/game/Weapon.java
+++ src/main/java/mech/mania/engine/game/Weapon.java
@@ -1,5 +1,9 @@
 package mech.mania.engine.game;
 
+import java.util.List;
+
 public class Weapon extends Wearable {
-    private int damageModifier = 0;
+    private int range = 0;
+    private int damage = 0;
+    //private List<effect> effectList;
 }
