bf85360128b31ec6e26cdc8e5dc2cb35e3464bac
Shaw Kagawa <skagawa2@illinois.edu>
2019-12-16 09:40:43 -0600

    added color to GameLogger, deleted GameLogic since GameStateController does same thing

--- src/main/java/mech/mania/engine/logging/GameLogger.java
+++ src/main/java/mech/mania/engine/logging/GameLogger.java
@@ -7,20 +7,29 @@ public class GameLogger {
         DEBUG
     }
 
-    public static void log(LogLevel level, String msg) {
-        // TODO: add colors to print statements
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_BLACK = "\u001B[30m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+    private static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void log(LogLevel level, String label, String msg) {
         switch (level) {
             case INFO:
-
+                System.out.print(ANSI_WHITE);
                 break;
             case ERROR:
-
+                System.out.print(ANSI_RED);
                 break;
             case DEBUG:
-
+                System.out.print(ANSI_GREEN);
                 break;
         }
-
-        System.out.println(msg);
+        System.out.print("[" + label + "] " + msg);
+        System.out.println(ANSI_RESET);
     }
 }
