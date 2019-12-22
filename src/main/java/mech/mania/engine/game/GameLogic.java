207e14ac926105fa20b99e07a2ca8bfb29d9b44a
Matthew Pham <matthewpham55@gmail.com>
2019-12-21 22:22:24 -0800

    initial validate attack

--- src/main/java/mech/mania/engine/game/GameLogic.java
+++ src/main/java/mech/mania/engine/game/GameLogic.java
@@ -3,7 +3,7 @@ package mech.mania.engine.game;
 import mech.mania.engine.server.playercommunication.PlayerDecision;
 import mech.mania.engine.server.visualizercommunication.VisualizerBinaryWebSocketHandler;
 import mech.mania.engine.server.visualizercommunication.VisualizerTurnProtos.VisualizerTurn;
-
+import mech.mania.engine.game.items.Item;
 import java.util.List;
 
 /**
@@ -23,4 +23,48 @@ public class GameLogic {
         VisualizerBinaryWebSocketHandler.sendTurn(visualizerTurn.build());
         return null;
     }
+    /*
+    Player has weapon in inv
+    coord is valid (map, within range)
+     */
+    public boolean validateAttack(PlayerDecision p){
+
+        Item weapon = null;
+        Item[] playersInventory = new Item[1];
+
+        boolean hasWeapon = false;
+
+        for(Item i : playersInventory){
+            if(weapon.equals(i)){
+                hasWeapon = true;
+                break;
+            }
+        }
+
+        if(!hasWeapon){
+            //throw new InvalidWeaponException
+        }
+
+       // boolean inMap = false;
+        int boardWidth = 1;
+        int boardHeight = 1;
+
+        int attackX = 1;
+        int attackY = 1;
+
+        if(attackX >= boardHeight || attackY >= boardWidth{
+            //throw new AttackOutOfBoundsException
+        }
+
+        double range = 1;
+        int currentX = 1;
+        int currentY = 1;
+
+        if(Math.sqrt(Math.pow(currentX - attackX,2) + Math.pow(currentY - attackY,2)) > range){
+            //throw new AttackOutOfRangeException
+        }
+
+        return false;
+    }
+
 }
