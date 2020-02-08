b0a2b690cb1b5436b7043583d2e5c016eb43a6b0
Sahil Patel <sahilshahpatel@gmail.com>
2020-02-08 17:23:18 -0600

    Changed StatusModifier and TempStatusModifier classes so that damagePerTurn (DOTs) only exists in TempStatusModifier.

--- src/main/java/mech/mania/engine/game/items/TempStatusModifier.java
+++ src/main/java/mech/mania/engine/game/items/TempStatusModifier.java
@@ -1,13 +1,15 @@
 package mech.mania.engine.game.items;
 
 public class TempStatusModifier extends StatusModifier {
-    int duration;
+    private int duration;
+    private double damagePerTurn;               // Concept: poison type of damage that effects player over time
 
     public TempStatusModifier(double speedChange, double healthChange, double experienceChange,
                               double magicDamageChange, double physicalDamageChange, double magicDefenseChange,
-                              double physicalDefenseChange, double damagePerTurn, int duration) {
+                              double physicalDefenseChange, int duration, double damagePerTurn) {
         super(speedChange, healthChange, experienceChange, magicDamageChange, physicalDamageChange, magicDefenseChange,
-                physicalDefenseChange, damagePerTurn);
+                physicalDefenseChange);
         this.duration = duration;
+        this.damagePerTurn = damagePerTurn;
     }
 }
