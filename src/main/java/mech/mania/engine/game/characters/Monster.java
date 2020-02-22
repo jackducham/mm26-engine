409e38fac9eceda6a405bac8d2ce45248a4547b3
nikash.walia@gmail.com <omgohmygod123!@#>
2020-02-22 16:31:09 -0600

    Initial monster functionality

--- src/main/java/mech/mania/engine/game/characters/Monster.java
+++ src/main/java/mech/mania/engine/game/characters/Monster.java
@@ -1,13 +1,24 @@
 package mech.mania.engine.game.characters;
 
+import java.util.HashMap;
+import java.util.Map;
 import mech.mania.engine.game.items.Item;
 
 import java.util.List;
 
 public class Monster extends Character {
     private List<Item> drops;
+    private Map<Player, Double> taggedPlayersDamage;
 
     public Monster(List<Item> drops) {
         this.drops = drops;
+        taggedPlayersDamage = new HashMap<Player, Double>();
     }
+
+    public void takeDamage(double damage, Player player) {
+        if (taggedPlayersDamage.containsKey(player)) {
+            taggedPlayersDamage.put(player, taggedPlayersDamage.get(player) + damage);
+        }
+    }
+
 }
