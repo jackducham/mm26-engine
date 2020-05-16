71c64187f3e3246823b7f4016c208a87af2464de
werdwar <drewlw2@illinois.edu>
2020-05-16 14:29:53 -0500

    split damage per turn in temp modifier

--- src/main/java/mech/mania/engine/game/items/TempStatusModifier.java
+++ src/main/java/mech/mania/engine/game/items/TempStatusModifier.java
@@ -2,19 +2,25 @@ package mech.mania.engine.game.items;
 
 public class TempStatusModifier extends StatusModifier {
     private int duration;
-    private int damagePerTurn;
+    private int magicDamagePerTurn;
+    private int physicalDamagePerTurn;
     /* damagePerTurn is a flat amount of damage delt to the character each turn,
      * which is still affected by damageChange and percentDamageChange.
      * (either physical [for bleeding] or magic [for poison], whichever set applies)
      */
 
-    public TempStatusModifier(int speedChange, int healthChange, int experienceChange,
-                              int magicDamageChange, int physicalDamageChange, int magicDefenseChange,
-                              int physicalDefenseChange, int duration, int damagePerTurn) {
-        super(speedChange, healthChange, experienceChange, magicDamageChange, physicalDamageChange, magicDefenseChange,
-                physicalDefenseChange);
+    public TempStatusModifier(int speedChange, double percentSpeedChange, int healthChange, double percentHealthChange,
+                              int experienceChange, double percentExperienceChange, int magicDamageChange,
+                              double percentMagicDamageChange, int physicalDamageChange,
+                              double percentPhysicalDamageChange, int magicDefenseChange,
+                              double percentMagicDefenseChange, int physicalDefenseChange,
+                              double percentPhysicalDefenseChange, int regenPerTurn, int duration, int magicDamagePerTurn, int physicalDamagePerTurn) {
+        super(speedChange, percentSpeedChange, healthChange, percentHealthChange, experienceChange, percentExperienceChange,
+                magicDamageChange, percentMagicDamageChange, physicalDamageChange, percentPhysicalDamageChange,
+                magicDefenseChange, percentMagicDefenseChange, physicalDefenseChange, percentPhysicalDefenseChange, regenPerTurn);
         this.duration = duration;
-        this.damagePerTurn = damagePerTurn;
+        this.magicDamagePerTurn = magicDamagePerTurn;
+        this.physicalDamagePerTurn = physicalDamagePerTurn;
     }
 
     public void updateTurnsLeft() {
@@ -28,15 +34,24 @@ public class TempStatusModifier extends StatusModifier {
     public TempStatusModifier(ItemProtos.TempStatusModifier tempStatusModifierProto) {
         super(
                 tempStatusModifierProto.getStats().getSpeedChange(),
+                tempStatusModifierProto.getStats().getPercentSpeedChange(),
                 tempStatusModifierProto.getStats().getHealthChange(),
+                tempStatusModifierProto.getStats().getPercentHealthChange(),
                 tempStatusModifierProto.getStats().getExperienceChange(),
+                tempStatusModifierProto.getStats().getPercentExperienceChange(),
                 tempStatusModifierProto.getStats().getMagicDamageChange(),
+                tempStatusModifierProto.getStats().getPercentMagicDamageChange(),
                 tempStatusModifierProto.getStats().getPhysicalDamageChange(),
+                tempStatusModifierProto.getStats().getPercentPhysicalDamageChange(),
                 tempStatusModifierProto.getStats().getMagicDefenseChange(),
+                tempStatusModifierProto.getStats().getPercentMagicDefenseChange(),
                 tempStatusModifierProto.getStats().getPhysicalDefenseChange()
+                tempStatusModifierProto.getStats().getPercentPhysicalDefenseChange(),
+                tempStatusModifierProto.getStats().getRegenPerTurn()
         );
         this.duration = tempStatusModifierProto.getDuration();
-        this.damagePerTurn = tempStatusModifierProto.getDamagePerTurn();
+        this.magicDamagePerTurn = tempStatusModifierProto.getMagicDamagePerTurn();
+        this.physicalDamagePerTurn = tempStatusModifierProto.getPhysicalDamagePerTurn();
     }
 
     public ItemProtos.TempStatusModifier buildProtoClassTemp() {
