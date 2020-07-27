64a3975640c70e9ea53cf333c949b8ab25a01203
werdwar <drewlw2@illinois.edu>
2020-07-26 22:41:01 -0500

    added documentation to the Clothes class as well as the create default Clothes function

--- src/main/java/mech/mania/engine/game/items/Clothes.java
+++ src/main/java/mech/mania/engine/game/items/Clothes.java
@@ -1,14 +1,26 @@
 package mech.mania.engine.game.items;
 
 public class Clothes extends Wearable {
+    /**
+     * Creates a Clothes object with the given stats.
+     * @param stats the stats the Clothes will be created with
+     */
     public Clothes(StatusModifier stats) {
         super(stats);
     }
 
+    /**
+     * Creates a Clothes object based on a Protocol Buffer.
+     * @param clothesProto the protocol buffer to be copied
+     */
     public Clothes(ItemProtos.Clothes clothesProto) {
         super(new StatusModifier(clothesProto.getStats()));
     }
 
+    /**
+     * Creates a clothes Protocol Buffer based on the Clothes instance this function is called on.
+     * @return a clothes protocol buffer representing the Clothes object.
+     */
     public ItemProtos.Clothes buildProtoClassClothes() {
         ItemProtos.Clothes.Builder clothesBuilder = ItemProtos.Clothes.newBuilder();
         clothesBuilder.setStats(stats.buildProtoClass());
@@ -16,6 +28,10 @@ public class Clothes extends Wearable {
         return clothesBuilder.build();
     }
 
+    /**
+     * Creates an item Protocol Buffer based on the Clothes instance this function is called on.
+     * @return an item protocol buffer representing the Clothes object.
+     */
     public ItemProtos.Item buildProtoClassItem() {
         ItemProtos.Clothes clothesProtoClass = buildProtoClassClothes();
 
@@ -23,5 +39,16 @@ public class Clothes extends Wearable {
         itemBuilder.setClothes(clothesProtoClass);
 
         return itemBuilder.build();
+    }
+
+    /**
+     * Creates a default Clothes for use in testing. NOTE: Each default item provides a particular stat increase;
+     * the default clothes provides a bonus of 8 to the defense stat.
+     * @return a default Clothes object
+     */
+    public static Clothes createDefaultClothes() {
+        StatusModifier defaultStatusModifier = new StatusModifier(0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0);
+        Clothes defaultClothes = new Clothes(defaultStatusModifier);
+        return defaultClothes;
     }
 }
