9bca5d2a95434447d1c79cc7471983b5ecd4759d
Sahil Patel <sahilshahpatel@gmail.com>
2020-08-10 11:18:33 -0500

    Added more test cases Made player base stats final

--- src/main/java/mech/mania/engine/domain/game/characters/Player.java
+++ src/main/java/mech/mania/engine/domain/game/characters/Player.java
@@ -12,10 +12,10 @@ public class Player extends Character {
     private Shoes shoes;
     private Item[] inventory;
 
-    private static int BASE_SPEED = 5;
-    private static int BASE_MAX_HEALTH = 20;
-    private static int BASE_ATTACK = 0;
-    private static int BASE_DEFENSE = 0;
+    private static final int BASE_SPEED = 5;
+    private static final int BASE_MAX_HEALTH = 20;
+    private static final int BASE_ATTACK = 0;
+    private static final int BASE_DEFENSE = 0;
 
     /**
      * Standard Constructor which uses default static values for speed, hp, atk, and def.
--- src/test/java/mech/mania/engine/domain/game/MovementTests.java
+++ src/test/java/mech/mania/engine/domain/game/MovementTests.java
@@ -3,16 +3,12 @@ package mech.mania.engine.domain.game;
 import mech.mania.engine.domain.game.characters.Position;
 import mech.mania.engine.domain.model.CharacterProtos;
 import mech.mania.engine.domain.model.PlayerProtos;
-import mech.mania.engine.domain.model.PlayerProtos.PlayerDecision;
-import mech.mania.engine.service_layer.UnitOfWorkAbstract;
-import mech.mania.engine.service_layer.UnitOfWorkFake;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 /** This contains tests for decisions related to movements */
@@ -43,7 +39,7 @@ public class MovementTests {
      * Tests MOVE decision (moves player from 0, 0 to 1, 0)
      */
     @Test
-    public void movePlayer(){
+    public void movePlayerToAdjacentEastPosition(){
         final int final_x = 1;
         final int final_y = 0;
 
@@ -67,4 +63,177 @@ public class MovementTests {
         assertTrue(finalPos.equals(expectedPos));
     }
 
+    /**
+     * Tests MOVE decision (moves player from 1, 0 to 0, 0)
+     */
+    @Test
+    public void movePlayerToAdjacentWestPosition(){
+        // Set player position to (1, 0)
+        gameState.getPlayer("player1").getPosition().setX(1);
+
+        final int final_x = 0;
+        final int final_y = 0;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(final_x, final_y, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
+
+    /**
+     * Tests MOVE decision (moves player from 0, 0 to 0, 1)
+     */
+    @Test
+    public void movePlayerToAdjacentSouthPosition(){
+        final int final_x = 0;
+        final int final_y = 1;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(final_x, final_y, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
+
+    /**
+     * Tests MOVE decision (moves player from 0, 1 to 0, 0)
+     */
+    @Test
+    public void movePlayerToAdjacentNorthPosition(){
+        // Set player position to (0, 1)
+        gameState.getPlayer("player1").getPosition().setY(1);
+
+        final int final_x = 0;
+        final int final_y = 0;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(final_x, final_y, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
+
+    /**
+     * Tests MOVE decision (moves player from 0, 0 to 0, 2)
+     */
+    @Test
+    public void movePlayerStraightSouth(){
+        final int final_x = 0;
+        final int final_y = 2;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(final_x, final_y, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
+
+    /**
+     * Tests MOVE decision (moves player from 0, 0 to 2, 0)
+     */
+    @Test
+    public void movePlayerStraightEast(){
+        final int final_x = 2;
+        final int final_y = 0;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(final_x, final_y, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
+
+    /**
+     * Tests MOVE decision (attempts to move player from 0, 0 to 15, 0 but fails due to speed check)
+     */
+    @Test
+    public void movePlayerMoreThanSpeed(){
+        final int final_x = 15;
+        final int final_y = 0;
+
+        // Move player1 to 1, 0
+        PlayerProtos.PlayerDecision.Builder decision = PlayerProtos.PlayerDecision.newBuilder();
+        decision.setDecisionType(CharacterProtos.DecisionType.MOVE);
+
+        CharacterProtos.Position.Builder newPos = CharacterProtos.Position.newBuilder();
+        newPos.setX(final_x).setY(final_y).setBoardId("player1");
+        decision.setTargetPosition(newPos.build());
+
+        // Execute decision
+        HashMap<String, PlayerProtos.PlayerDecision> decisionMap = new HashMap<>();
+        decisionMap.put("player1", decision.build());
+        GameLogic.doTurn(gameState, decisionMap);
+
+        // Check that player has been moved
+        Position finalPos = gameState.getPlayer("player1").getPosition();
+        System.out.println("Final Position: (" + finalPos.getX() + ", " + finalPos.getY() + ", " + finalPos.getBoardID() + ")");
+        Position expectedPos = new Position(0, 0, "player1");
+        assertTrue(finalPos.equals(expectedPos));
+    }
 }
