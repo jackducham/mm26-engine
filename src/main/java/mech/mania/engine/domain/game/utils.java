f0ecad773c5e760bb2694aa12222b437f579925c
allenc3 <allenc3@illinois.edu>
2020-08-31 23:02:01 -0500

    1 last api left

---
+++ src/main/java/mech/mania/engine/domain/game/utils.java
@@ -0,0 +1,159 @@
+package mech.mania.engine.domain.game;
+
+import mech.mania.engine.domain.game.board.Board;
+import mech.mania.engine.domain.game.characters.Character;
+import mech.mania.engine.domain.game.characters.Monster;
+import mech.mania.engine.domain.game.characters.Player;
+import mech.mania.engine.domain.game.characters.Position;
+import mech.mania.engine.domain.game.items.Weapon;
+
+import java.util.*;
+
+public class utils {
+
+    public static List<Character> findEnemies(GameState gameState, String playerName) {
+        Player player = gameState.getPlayer(playerName);
+
+        List<AbstractMap.SimpleEntry<Double, Character>> enemiesDist = new ArrayList<>();
+        for (Map.Entry<String, Player> entry : gameState.getAllPlayers().entrySet()) {
+            String otherName = entry.getKey();
+            Player other = entry.getValue();
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName)) {
+                enemiesDist.add(new AbstractMap.SimpleEntry<Double, Character>(player.getPosition().distance(other.getPosition()), (Character) other));
+            }
+        }
+        for (Map.Entry<String, Monster> entry : gameState.getAllMonsters().entrySet()) {
+            String otherName = entry.getKey();
+            Monster other = entry.getValue();
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName)) {
+                enemiesDist.add(new AbstractMap.SimpleEntry<Double, Character>(player.getPosition().distance(other.getPosition()), (Character) other));
+            }
+        }
+
+        Comparator<AbstractMap.SimpleEntry<Double, Character>> compareByDistance = (AbstractMap.SimpleEntry<Double, Character> d1, AbstractMap.SimpleEntry<Double, Character> d2)
+                -> d1.getKey().compareTo( d2.getKey() );
+        Collections.sort(enemiesDist, compareByDistance);
+        List<Character> enemies = new ArrayList<>();
+        for (AbstractMap.SimpleEntry<Double, Character> dist : enemiesDist) {
+            enemies.add(dist.getValue());
+        }
+        return enemies;
+    }
+
+    public static List<Character> findEnemiesInRange(GameState gameState, String playerName) {
+        Player player = gameState.getPlayer(playerName);
+        Weapon weapon = player.getWeapon();
+
+        List<AbstractMap.SimpleEntry<Double, Character>> enemiesDist = new ArrayList<>();
+        for (Map.Entry<String, Player> entry : gameState.getAllPlayers().entrySet()) {
+            String otherName = entry.getKey();
+            Player other = entry.getValue();
+            Double distance = player.getPosition().distance(other.getPosition());
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName) && distance <= (double) weapon.getRange()) {
+                enemiesDist.add(new AbstractMap.SimpleEntry<Double, Character>(distance, (Character) other));
+            }
+        }
+        for (Map.Entry<String, Monster> entry : gameState.getAllMonsters().entrySet()) {
+            String otherName = entry.getKey();
+            Monster other = entry.getValue();
+            Double distance = player.getPosition().distance(other.getPosition());
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName) && distance <= (double) weapon.getRange()) {
+                enemiesDist.add(new AbstractMap.SimpleEntry<Double, Character>(distance, (Character) other));
+            }
+        }
+
+        Comparator<AbstractMap.SimpleEntry<Double, Character>> compareByDistance = (AbstractMap.SimpleEntry<Double, Character> d1, AbstractMap.SimpleEntry<Double, Character> d2)
+                -> d1.getKey().compareTo( d2.getKey() );
+        Collections.sort(enemiesDist, compareByDistance);
+        List<Character> enemies = new ArrayList<>();
+        for (AbstractMap.SimpleEntry<Double, Character> dist : enemiesDist) {
+            enemies.add(dist.getValue());
+        }
+        return enemies;
+    }
+
+    public static List<Character> findAllEnemiesHit(GameState gameState, String playerName, Position targetSpot) {
+        Player player = gameState.getPlayer(playerName);
+        Weapon weapon = player.getWeapon();
+
+        List<Character> enemies = new ArrayList<>();
+        for (Map.Entry<String, Player> entry : gameState.getAllPlayers().entrySet()) {
+            String otherName = entry.getKey();
+            Player other = entry.getValue();
+            Double distance = targetSpot.distance(other.getPosition());
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName) && distance <= (double) weapon.getRange()) {
+                enemies.add(other);
+            }
+        }
+        for (Map.Entry<String, Monster> entry : gameState.getAllMonsters().entrySet()) {
+            String otherName = entry.getKey();
+            Monster other = entry.getValue();
+            Double distance = targetSpot.distance(other.getPosition());
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID() && !otherName.equals(playerName) && distance <= (double) weapon.getRange()) {
+                enemies.add(other);
+            }
+        }
+        return enemies;
+    }
+
+    public static List<Monster> findMonsters(GameState gameState, String playerName) {
+        Player player = gameState.getPlayer(playerName);
+
+        List<Monster> monsters = new ArrayList<>();
+        for (Map.Entry<String, Monster> entry : gameState.getAllMonsters().entrySet()) {
+            String otherName = entry.getKey();
+            Monster other = entry.getValue();
+            if (other.getPosition().getBoardID() == player.getPosition().getBoardID()) {
+                monsters.add(other);
+            }
+        }
+
+        Comparator<Monster> compareByExp = (Monster m1, Monster m2)
+                -> Integer.valueOf(m1.getExperience()).compareTo( Integer.valueOf(m2.getExperience()) );
+        Collections.sort(monsters, compareByExp.reversed());
+
+        return monsters;
+    }
+
+    public static boolean canBeAttacked(GameState gameState, String playerName) {
+        Player player = gameState.getPlayer(playerName);
+
+        List<Character> enemies = findEnemies(gameState, playerName);
+        for (Character enemy : enemies) {
+            double dist = player.getPosition().distance(enemy.getPosition());
+            if (dist <= enemy.getWeapon().getRange()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Position findClosestPortal(GameState gameState, String playerName) {
+        Player player = gameState.getPlayer(playerName);
+        Board board = gameState.getBoard(player.getPosition().getBoardID());
+        List<Position> portals = board.getPortals();
+
+        Double minDist = null;
+        Position closestPortal = null;
+        for (Position portal : portals) {
+            Double dist = portal.distance(player.getPosition());
+            if (minDist == null) {
+                minDist = dist;
+                closestPortal = portal;
+            } else if (dist < minDist) {
+                minDist = dist;
+                closestPortal = portal;
+            }
+        }
+        return closestPortal;
+    }
+
+    public static List<Player> leaderBoard(GameState gameState) {
+        List<Player> players = new ArrayList<>(gameState.getAllPlayers().values());
+        Comparator<Player> compareByExp = (Player p1, Player p2)
+                -> Integer.valueOf(p1.getExperience()).compareTo( Integer.valueOf(p2.getExperience()) );
+        Collections.sort(players, compareByExp.reversed());
+
+        return players;
+    }
+}
--- src/main/java/mech/mania/engine/service_layer/APIRESTHandler.java
+++ src/main/java/mech/mania/engine/service_layer/APIRESTHandler.java
@@ -2,9 +2,8 @@ package mech.mania.engine.service_layer;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import mech.mania.engine.domain.game.characters.Character;
+import mech.mania.engine.domain.game.characters.Player;
 import mech.mania.engine.domain.game.characters.Position;
-import mech.mania.engine.domain.game.enemies.FindEnemiesInRange;
-import mech.mania.engine.domain.game.enemies.FindMonsters;
 import mech.mania.engine.domain.model.CharacterProtos;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,7 +13,7 @@ import mech.mania.engine.domain.model.ApiProtos;
 import mech.mania.engine.domain.game.pathfinding.PathFinder;
 import mech.mania.engine.domain.game.GameState;
 import mech.mania.engine.domain.game.characters.Monster;
-import mech.mania.engine.domain.game.enemies.FindEnemies;
+import mech.mania.engine.domain.game.utils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -88,7 +87,7 @@ public class APIRESTHandler {
             GameState gameState = new GameState(requestInfo.getGameState());
             String player_name = requestInfo.getPlayerName();
 
-            List<Character> enemies = FindEnemies.findEnemies(gameState, player_name);
+            List<Character> enemies = utils.findEnemies(gameState, player_name);
 
             ApiProtos.APIFindEnemiesResponse.Builder responseBuilder = ApiProtos.APIFindEnemiesResponse.newBuilder();
             List<CharacterProtos.Character> protoEnemies = new ArrayList<>();
@@ -120,7 +119,7 @@ public class APIRESTHandler {
             GameState gameState = new GameState(requestInfo.getGameState());
             String player_name = requestInfo.getPlayerName();
 
-            List<Monster> monsters = FindMonsters.findMonsters(gameState, player_name);
+            List<Monster> monsters = utils.findMonsters(gameState, player_name);
 
             ApiProtos.APIFindMonstersResponse.Builder responseBuilder = ApiProtos.APIFindMonstersResponse.newBuilder();
             List<CharacterProtos.Monster> protoMonsters = new ArrayList<>();
@@ -152,7 +151,7 @@ public class APIRESTHandler {
             GameState gameState = new GameState(requestInfo.getGameState());
             String player_name = requestInfo.getPlayerName();
 
-            List<Character> enemies = FindEnemiesInRange.findEnemiesInRange(gameState, player_name);
+            List<Character> enemies = utils.findEnemiesInRange(gameState, player_name);
 
             ApiProtos.APIFindEnemiesInRangeResponse.Builder responseBuilder = ApiProtos.APIFindEnemiesInRangeResponse.newBuilder();
             List<CharacterProtos.Character> protoEnemies = new ArrayList<>();
@@ -160,7 +159,7 @@ public class APIRESTHandler {
                 protoEnemies.add(enemy.buildProtoClassCharacter());
             }
             responseBuilder.addAllEnemies(protoEnemies);
-            LOGGER.fine(String.format("Successfully processed ApiFindEnemiesRequest."));
+            LOGGER.fine(String.format("Successfully processed ApiFindEnemiesInRange."));
 
             return responseBuilder.build().toByteArray();
 
@@ -168,6 +167,126 @@ public class APIRESTHandler {
             // log that error occurred
             LOGGER.warning("InvalidProtocolBufferException on /findEnemiesInRange request from player: " + e.getMessage());
             return ApiProtos.APIFindEnemiesInRangeResponse.newBuilder()
+                    .setStatus(ApiProtos.APIStatus.newBuilder()
+                            .setStatus(400)
+                            .setMessage("InvalidProtocolBufferException: " + e.getMessage())
+                            .build())
+                    .build()
+                    .toByteArray();
+        }
+    }
+
+    @GetMapping("/canBeAttacked")
+    public byte[] canBeAttacked(@RequestBody byte[] payload) {
+        try {
+            ApiProtos.APICanBeAttackedRequest requestInfo = ApiProtos.APICanBeAttackedRequest.parseFrom(payload);
+            GameState gameState = new GameState(requestInfo.getGameState());
+            String player_name = requestInfo.getPlayerName();
+
+            boolean canPlayerBeAttacked = utils.canBeAttacked(gameState, player_name);
+
+            ApiProtos.APICanBeAttackedResponse.Builder responseBuilder = ApiProtos.APICanBeAttackedResponse.newBuilder();
+            responseBuilder.setCanBeAttacked(canPlayerBeAttacked);
+            LOGGER.fine(String.format("Successfully processed ApiCanBeAttackedRequest."));
+
+            return responseBuilder.build().toByteArray();
+
+        } catch (InvalidProtocolBufferException e) {
+            // log that error occurred
+            LOGGER.warning("InvalidProtocolBufferException on /canBeAttacked request from player: " + e.getMessage());
+            return ApiProtos.APICanBeAttackedResponse.newBuilder()
+                    .setStatus(ApiProtos.APIStatus.newBuilder()
+                            .setStatus(400)
+                            .setMessage("InvalidProtocolBufferException: " + e.getMessage())
+                            .build())
+                    .build()
+                    .toByteArray();
+        }
+    }
+
+    @GetMapping("/findClosestPortal")
+    public byte[] getClosestPortal(@RequestBody byte[] payload) {
+        try {
+            ApiProtos.APIFindClosestPortalRequest requestInfo = ApiProtos.APIFindClosestPortalRequest.parseFrom(payload);
+            GameState gameState = new GameState(requestInfo.getGameState());
+            String player_name = requestInfo.getPlayerName();
+
+            Position portal = utils.findClosestPortal(gameState, player_name);
+
+            ApiProtos.APIFindClosestPortalResponse.Builder responseBuilder = ApiProtos.APIFindClosestPortalResponse.newBuilder();
+            responseBuilder.setPortal(portal.buildProtoClass());
+            LOGGER.fine(String.format("Successfully processed ApiFindClosestPortalRequest."));
+
+            return responseBuilder.build().toByteArray();
+
+        } catch (InvalidProtocolBufferException e) {
+            // log that error occurred
+            LOGGER.warning("InvalidProtocolBufferException on /findClosestPortal request from player: " + e.getMessage());
+            return ApiProtos.APIFindClosestPortalResponse.newBuilder()
+                    .setStatus(ApiProtos.APIStatus.newBuilder()
+                            .setStatus(400)
+                            .setMessage("InvalidProtocolBufferException: " + e.getMessage())
+                            .build())
+                    .build()
+                    .toByteArray();
+        }
+    }
+
+    @GetMapping("/leaderBoard")
+    public byte[] getLeaderBoard(@RequestBody byte[] payload) {
+        try {
+            ApiProtos.APILeaderBoardRequest requestInfo = ApiProtos.APILeaderBoardRequest.parseFrom(payload);
+            GameState gameState = new GameState(requestInfo.getGameState());
+
+            List<Player> leaderBoard = utils.leaderBoard(gameState);
+
+            ApiProtos.APILeaderBoardResponse.Builder responseBuilder = ApiProtos.APILeaderBoardResponse.newBuilder();
+            List<CharacterProtos.Player> protoLeaderBoard = new ArrayList<>();
+            for (Player player : leaderBoard) {
+                protoLeaderBoard.add(player.buildProtoClassPlayer());
+            }
+            responseBuilder.addAllLeaderBoard(protoLeaderBoard);
+            LOGGER.fine(String.format("Successfully processed ApiLeaderBoardRequest."));
+
+            return responseBuilder.build().toByteArray();
+
+        } catch (InvalidProtocolBufferException e) {
+            // log that error occurred
+            LOGGER.warning("InvalidProtocolBufferException on /leaderBoard request from player: " + e.getMessage());
+            return ApiProtos.APILeaderBoardResponse.newBuilder()
+                    .setStatus(ApiProtos.APIStatus.newBuilder()
+                            .setStatus(400)
+                            .setMessage("InvalidProtocolBufferException: " + e.getMessage())
+                            .build())
+                    .build()
+                    .toByteArray();
+        }
+    }
+
+    @GetMapping("/findAllEnemiesHit")
+    public byte[] getAllEnemiesHit(@RequestBody byte[] payload) {
+        try {
+            ApiProtos.APIAllEnemiesHitRequest requestInfo = ApiProtos.APIAllEnemiesHitRequest.parseFrom(payload);
+            GameState gameState = new GameState(requestInfo.getGameState());
+            String playerName = requestInfo.getPlayerName();
+            Position targetSpot = new Position(requestInfo.getTargetSpot());
+
+            List<Character> allEnemiesHit= utils.findAllEnemiesHit(gameState, playerName, targetSpot);
+
+            ApiProtos.APIAllEnemiesHitResponse.Builder responseBuilder = ApiProtos.APIAllEnemiesHitResponse.newBuilder();
+            List<CharacterProtos.Character> protoAllEnemiesHit = new ArrayList<>();
+            for (Character character : allEnemiesHit) {
+                protoAllEnemiesHit.add(character.buildProtoClassCharacter());
+            }
+            responseBuilder.addAllEnemiesHit(protoAllEnemiesHit);
+            LOGGER.fine(String.format("Successfully processed ApiAllEnemiesHitRequest."));
+
+            return responseBuilder.build().toByteArray();
+
+        } catch (InvalidProtocolBufferException e) {
+            // log that error occurred
+            LOGGER.warning("InvalidProtocolBufferException on /findAllEnemiesHit request from player: " + e.getMessage());
+            return ApiProtos.APIAllEnemiesHitResponse.newBuilder()
                     .setStatus(ApiProtos.APIStatus.newBuilder()
                             .setStatus(400)
                             .setMessage("InvalidProtocolBufferException: " + e.getMessage())
