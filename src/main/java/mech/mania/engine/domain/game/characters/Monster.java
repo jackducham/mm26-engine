c794fe0491ba405c4116c2df043b13df6ec5a15d
Fgoyal <fgoyal2@illinois.edu>
2020-09-27 00:42:13 -0400

    Added isDead check for ai

--- src/main/java/mech/mania/engine/domain/game/characters/Monster.java
+++ src/main/java/mech/mania/engine/domain/game/characters/Monster.java
@@ -114,6 +114,9 @@ public class Monster extends Character {
         List<Character> inRange = GameLogic.findEnemiesInRangeByDistance(gameState, getPosition(), getName(), aggroRange);
         if(inRange != null) {
             for (Character character : inRange) {
+                if (character.isDead()) {
+                    continue;
+                }
                 if (!character.isDead() && character instanceof Player && !taggedPlayersDamage.containsKey(character.getName())) {
                     taggedPlayersDamage.put(character.getName(), 0);
                 }
